234c710b7cfff5575a623e048667b06c7d0aa1be
Julian Contreras <jrcontre@mit.edu>
2017-11-18 21:52:29 -0500

    basic location permission dialog

--- app/src/main/java/com/radix/nowplayinglog/fragments/SettingsFragment.java
+++ app/src/main/java/com/radix/nowplayinglog/fragments/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -15,6 +16,16 @@ import com.radix.nowplayinglog.R;
 
 public class SettingsFragment extends Fragment {
 
+  // Identifier for the permission request
+  private static final int READ_LOCATION_PERMISSIONS_REQUEST = 1;
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    getPermissionToReadLocation();
+  }
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
@@ -22,16 +33,13 @@ public class SettingsFragment extends Fragment {
     return inflater.inflate(R.layout.fragment_settings, container, false);
   }
 
-  // Identifier for the permission request
-  private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
-
-  public void getPermissionToReadUserContacts() {
-    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+  public void getPermissionToReadLocation() {
+    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
       // The permission is NOT already granted.
 
       // Fire off an async request to actually get the permission
-      requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
-          READ_CONTACTS_PERMISSIONS_REQUEST);
+      requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+          READ_LOCATION_PERMISSIONS_REQUEST);
     }
   }
 
@@ -41,18 +49,18 @@ public class SettingsFragment extends Fragment {
                                          @NonNull String permissions[],
                                          @NonNull int[] grantResults) {
     // Make sure it's our original READ_CONTACTS request
-    if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+    if (requestCode == READ_LOCATION_PERMISSIONS_REQUEST) {
       if (grantResults.length == 1 &&
           grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        Toast.makeText(getContext(), "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "Read Location permission granted", Toast.LENGTH_SHORT).show();
       } else {
         // showRationale = false if user clicks Never Ask Again, otherwise true
-        boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS);
+        boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION);
 
         if (showRationale) {
           // do something here to handle degraded mode
         } else {
-          Toast.makeText(getContext(), "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+          Toast.makeText(getContext(), "Read Location permission denied", Toast.LENGTH_SHORT).show();
         }
       }
     } else {
