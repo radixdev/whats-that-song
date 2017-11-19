7c0bdc5f5ac2e528c57eec2acd4a83d9c36ccb7d
Julian Contreras <julian.r.contreras@gmail.com>
2017-11-19 15:15:31 -0500

    map finally getting positioned

--- app/src/main/java/com/radix/nowplayinglog/fragments/SongMapFragment.java
+++ app/src/main/java/com/radix/nowplayinglog/fragments/SongMapFragment.java
@@ -6,14 +6,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.radix.nowplayinglog.R;
 
-public class SongMapFragment extends Fragment {
+public class SongMapFragment extends Fragment implements OnMapReadyCallback {
+  private GoogleMap mMap;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_map, container, false);
+    final View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_google_map);
+    mapFragment.getMapAsync(this);
+
+    return rootView;
+  }
+
+  /**
+   * Manipulates the map once available.
+   * This callback is triggered when the map is ready to be used.
+   * This is where we can add markers or lines, add listeners or move the camera. In this case,
+   * we just add a marker near Sydney, Australia.
+   * If Google Play services is not installed on the device, the user will be prompted to install
+   * it inside the SupportMapFragment. This method will only be triggered once the user has
+   * installed Google Play services and returned to the app.
+   */
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    mMap = googleMap;
+
+    // Add a marker in Sydney and move the camera
+    LatLng sydney = new LatLng(-34, 151);
+    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
   }
 }
