ef3de898b04eaef7ce08afeb6d7a3246e8b5b6be
Julian Contreras <julian.r.contreras@gmail.com>
2017-11-19 16:15:57 -0500

    added music note thing

--- app/src/main/java/com/radix/nowplayinglog/fragments/SongMapFragment.java
+++ app/src/main/java/com/radix/nowplayinglog/fragments/SongMapFragment.java
@@ -18,6 +18,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -109,9 +111,12 @@ public class SongMapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     LatLng songPosition = new LatLng(song.getLatitude(), song.getLongitude());
+    BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.ic_music_note_white_48dp);
+
     mMap.addMarker(new MarkerOptions()
         .position(songPosition)
         .title(song.getTitleAndArtistForDisplay())
+        .icon(bitmapDescriptor)
     );
   }
 }
